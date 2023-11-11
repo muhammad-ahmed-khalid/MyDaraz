1e87df38a02b82a6e8cfb836de18070ca849abb5
Ahmed Khalid <akhalid24.ak87@gmail.com>
2023-11-11 11:20:55 -0800

    mmkv storage Key Name Chaned

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -5,82 +5,82 @@ import {
     Text,
     TouchableOpacity,
     View,
-  } from 'react-native';
+} from 'react-native';
 import { clearAllData, getItem, setItem } from '../../services/storageService';
 import { useBoundStore } from '../../stores';
 import Card from '../../components/Card/Card';
 import { CARD_DUMMY_DATA } from '../../constants/DummyData';
 
 const Home = () => {
-    const storedValue = getItem("aojani");
+    const storedValue = getItem("cartItem");
     const [myCount, setMyCount] = useState(storedValue);
     useEffect(() => {
         setAddToCartZustand(storedValue)
-    },[])
+    }, [])
 
     const {
         setAddToCartZustand,
         addToCartZustand
-      } =useBoundStore()
+    } = useBoundStore()
     const handlePressChange = (price) => {
-      console.log("Ahmed", addToCartZustand + price);
-      setItem("aojani", addToCartZustand + price);
-      setMyCount(price);
-      setAddToCartZustand(addToCartZustand + price)
-    };
-  
-    const handlePressClear = () => {
-      console.log("Ahmed", null);
-      clearAllData()
-      setMyCount(0);
-      setAddToCartZustand(0)
+        console.log("Ahmed", addToCartZustand + price);
+        setItem("cartItem", addToCartZustand + price);
+        setMyCount(price);
+        setAddToCartZustand(addToCartZustand + price)
     };
 
-const handlePressCallBack = (val) => {
-    handlePressChange(val?.productPrice)
-}
-const handlePressRemovedCart = (price) => {
-    console.log("Ahmed", addToCartZustand - price);
-    if(addToCartZustand < price){
-        setItem("aojani", 0);
+    const handlePressClear = () => {
+        console.log("Ahmed", null);
+        clearAllData()
         setMyCount(0);
         setAddToCartZustand(0)
-    }
-    else{
-        setItem("aojani", addToCartZustand - price);
-    setMyCount(price);
-    setAddToCartZustand(addToCartZustand - price)
-    }
-    
-  };
-const handlePressRemoveCart = (val) => {
-    handlePressRemovedCart(val?.productPrice)
-}
+    };
 
-const renderItem = ({item}) => {
-    return(
-        <Card item={item} cb={handlePressCallBack} cbRemoveCart = {handlePressRemoveCart}/>
-    )
-}
+    const handlePressCallBack = (val) => {
+        handlePressChange(val?.productPrice)
+    }
+    const handlePressRemovedCart = (price) => {
+        console.log("Ahmed", addToCartZustand - price);
+        if (addToCartZustand < price) {
+            setItem("cartItem", 0);
+            setMyCount(0);
+            setAddToCartZustand(0)
+        }
+        else {
+            setItem("cartItem", addToCartZustand - price);
+            setMyCount(price);
+            setAddToCartZustand(addToCartZustand - price)
+        }
 
-  return (
-    <View style={styles.root}>
-          <TouchableOpacity onPress={handlePressClear}>
-        <Text>Clear</Text>
-      </TouchableOpacity> 
-        <FlatList 
-        data={CARD_DUMMY_DATA}
-        renderItem={renderItem}
-        />
-    {/* <Text>This is Ahmed{myCount}</Text>
+    };
+    const handlePressRemoveCart = (val) => {
+        handlePressRemovedCart(val?.productPrice)
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+            <Card item={item} cb={handlePressCallBack} cbRemoveCart={handlePressRemoveCart} />
+        )
+    }
+
+    return (
+        <View style={styles.root}>
+            <TouchableOpacity onPress={handlePressClear}>
+                <Text>Clear</Text>
+            </TouchableOpacity>
+            <FlatList
+                data={CARD_DUMMY_DATA}
+                renderItem={renderItem}
+            />
+            {/* <Text>This is Ahmed{myCount}</Text>
       <TouchableOpacity onPress={handlePressChange}>
         <Text>Change</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={handlePressClear}>
         <Text>Clear</Text>
       </TouchableOpacity> */}
-    </View>
-  )
+        </View>
+    )
 }
 
 export default Home
@@ -90,5 +90,5 @@ const styles = StyleSheet.create({
         flex: 1,
         // alignItems: 'center',
         // justifyContent: 'center'
-      }
+    }
 })
